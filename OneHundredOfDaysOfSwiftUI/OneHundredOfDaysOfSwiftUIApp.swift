d09a491fbd2dcc557a9f0a92872e8903781e98ff
james26k <cb26.hayashi@gmail.com>
2021-12-16 21:15:53 +0900

    create project

---
+++ OneHundredOfDaysOfSwiftUI/OneHundredOfDaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneHundredOfDaysOfSwiftUIApp.swift
+//  OneHundredOfDaysOfSwiftUI
+//
+//  Created by Kohei Hayashi on 2021/12/16.
+//
+
+import SwiftUI
+
+@main
+struct OneHundredOfDaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
