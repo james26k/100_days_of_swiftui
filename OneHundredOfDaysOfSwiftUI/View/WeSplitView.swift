9db2acd03c29cc284c2e1e256b87ccf11a53f6c6
james26k <cb26.hayashi@gmail.com>
2021-12-17 00:11:06 +0900

    add: add WeSplitView.swift

---
+++ OneHundredOfDaysOfSwiftUI/View/WeSplitView.swift
@@ -0,0 +1,20 @@
+//
+//  WeSplitView.swift
+//  OneHundredOfDaysOfSwiftUI
+//
+//  Created by Kohei Hayashi on 2021/12/17.
+//
+
+import SwiftUI
+
+struct WeSplitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeSplitView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeSplitView()
+    }
+}
