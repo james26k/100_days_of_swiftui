4fcd272329fb2459e01a13d3449f2e50a8d4feff
james26k <cb26.hayashi@gmail.com>
2021-12-25 19:43:13 +0900

    add: [WeSplitView] チュートリアルで利用するプロパティを追加

--- OneHundredOfDaysOfSwiftUI/View/Module/WeSplitView.swift
+++ OneHundredOfDaysOfSwiftUI/View/Module/WeSplitView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct WeSplitView: View {
+    private let tipPercentages = [10, 15, 20, 25, 0]
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
