df829492ef8c8129d6aad96de591751b3fd914c6
james26k <cb26.hayashi@gmail.com>
2021-12-27 23:46:05 +0900

    add: [project2] 二つ目のプロジェクト定義をProjectに追加

--- OneHundredOfDaysOfSwiftUI/Constants/Project.swift
+++ OneHundredOfDaysOfSwiftUI/Constants/Project.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 enum Project: CaseIterable {
     case weSplit
+    case guessTheFlag
 
     var name: String {
         switch self {
-        case .weSplit: return "We Split"
+        case .weSplit:      return "We Split"
+        case .guessTheFlag: return "Guess the flag"
         }
     }
 
     var view: some View {
         switch self {
         case .weSplit: return WeSplitView()
+        case .guessTheFlag: fatalError("Unimplemented")
         }
     }
 }
