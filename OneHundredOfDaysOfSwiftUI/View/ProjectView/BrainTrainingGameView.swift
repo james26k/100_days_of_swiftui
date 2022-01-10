eb289aaf076133661a52b3713b12d521da2b184c
james26k <cb26.hayashi@gmail.com>
2022-01-10 19:55:10 +0900

    add: [review] プロパティ追加、UI仮実装

--- OneHundredOfDaysOfSwiftUI/View/ProjectView/BrainTrainingGameView.swift
+++ OneHundredOfDaysOfSwiftUI/View/ProjectView/BrainTrainingGameView.swift
@@ -7,14 +7,65 @@
 
 import SwiftUI
 
-struct BrainTrainingGameView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationTitle(Project.brainTrainingGame.name)
-            .navigationBarTitleDisplayMode(.inline)
-    }
+private enum Move: String, CaseIterable {
+    case rock     = "✊"
+    case paper    = "✋"
+    case scissors = "✌️"
 }
 
+struct BrainTrainingGameView: View {
+    private let allMoves = Move.allCases
+    @State private var appsMove = Int.random(in: 0..<3)
+    @State private var shouldWin = Bool.random()
+
+    @State private var playersScore = 0
+    @State private var matchCount = 0
+    @State private var showingScoreAlert = false
+
+    var body: some View {
+        VStack(spacing: 50) {
+            VStack(spacing: 20) {
+                Text(allMoves[appsMove].rawValue)
+                Text("You should \(shouldWin ? "win" : "lose")")
+            }
+            .font(.title)
+
+            HStack(spacing: 50) {
+                ForEach(allMoves, id: \.self) {
+                    MoveButton(move: $0)
+                }
+            }
+        }
+        .navigationTitle(Project.brainTrainingGame.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+// MARK: - custom struct
+private struct MoveButton: View {
+    var move: Move
+    var didTapButton: ((Move) -> Void)?
+
+    var body: some View {
+        Button {
+            print("did tap \(move.rawValue)")
+            didTapButton?(move)
+        } label: {
+            HStack {
+                Text(move.rawValue)
+                    .font(.system(size: 50))
+            }
+        }
+        .buttonStyle(ScaleButtonStyle())
+    }
+}
+// MARK: - custom button style
+struct ScaleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 1.5 : 1)
+    }
+}
+// MARK: - preview
 struct BrainTrainingGameView_Previews: PreviewProvider {
     static var previews: some View {
         BrainTrainingGameView()
