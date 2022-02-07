e81374bfdcb4ee59db4eaa705f2c237f3421df46
james26k <cb26.hayashi@gmail.com>
2022-02-08 00:00:40 +0900

    add: [project4] 起きる時間を入力するためのDatePickerを追加

--- OneHundredOfDaysOfSwiftUI/View/ProjectView/BetterRestView.swift
+++ OneHundredOfDaysOfSwiftUI/View/ProjectView/BetterRestView.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct BetterRestView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
 
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours",
-                value: $sleepAmount,
-                in: 4...12,
-                step: 0.25)
+        VStack(spacing: 10) {
+            Stepper("\(sleepAmount.formatted()) hours",
+                    value: $sleepAmount,
+                    in: 4...12,
+                    step: 0.25)
+
+            DatePicker("Please enter a date",
+                       selection: $wakeUp,
+                       in: Date.now...)
+                .labelsHidden()
+        }
+        .padding(.horizontal)
     }
 }
 
