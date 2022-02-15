095c3b7ed06ca9f1304e474f35c23f8a742b7479
james26k <cb26.hayashi@gmail.com>
2022-02-16 01:11:13 +0900

    feat: [project4] CoreMLを利用して推奨就寝時刻を表示する機能を実装

--- OneHundredOfDaysOfSwiftUI/View/ProjectView/BetterRestView.swift
+++ OneHundredOfDaysOfSwiftUI/View/ProjectView/BetterRestView.swift
@@ -5,12 +5,17 @@
 //  Created by Kohei Hayashi on 2022/01/11.
 //
 
+import CoreML
 import SwiftUI
 
 struct BetterRestView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
 
     var body: some View {
         VStack {
@@ -37,6 +42,11 @@ struct BetterRestView: View {
                     value: $coffeeAmount,
                     in: 1...20)
         }
+        .alert(alertTitle, isPresented: $showingAlert) {
+            Button("OK") {}
+        } message: {
+            Text(alertMessage)
+        }
         .navigationTitle(Project.betterRest.name)
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
@@ -47,8 +57,24 @@ struct BetterRestView: View {
 
 fileprivate extension BetterRestView {
     func calculateBedtime() {
-        // TODO: implement action
-        print("did tap calculate button")
+        do {
+            let config = MLModelConfiguration()
+            let model = try BetterRest(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour + minute),
+                                                  estimatedSleep: sleepAmount,
+                                                  coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
