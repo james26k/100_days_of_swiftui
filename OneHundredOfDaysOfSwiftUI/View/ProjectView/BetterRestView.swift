2101e045613784e0fe64d23df8cf92c208c9ddef
james26k <cb26.hayashi@gmail.com>
2022-02-16 00:28:41 +0900

    update: [project4] 希望起床時刻、希望睡眠時間、日々のコーヒー摂取量を入力できるようUI更新

--- OneHundredOfDaysOfSwiftUI/View/ProjectView/BetterRestView.swift
+++ OneHundredOfDaysOfSwiftUI/View/ProjectView/BetterRestView.swift
@@ -8,22 +8,35 @@
 import SwiftUI
 
 struct BetterRestView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
 
     var body: some View {
-        VStack(spacing: 10) {
+        VStack {
+            Text("When do you want to wake up?")
+                .font(.headline)
+
+            DatePicker("Please enter a time",
+                       selection: $wakeUp,
+                       displayedComponents: .hourAndMinute)
+                .labelsHidden()
+
+            Text("Desired amount of sleep")
+                .font(.headline)
+
             Stepper("\(sleepAmount.formatted()) hours",
                     value: $sleepAmount,
                     in: 4...12,
                     step: 0.25)
 
-            DatePicker("Please enter a date",
-                       selection: $wakeUp,
-                       in: Date.now...)
-                .labelsHidden()
+            Text("Daily coffee intake")
+                .font(.headline)
+
+            Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+                    value: $coffeeAmount,
+                    in: 1...20)
         }
-        .padding(.horizontal)
     }
 }
 
