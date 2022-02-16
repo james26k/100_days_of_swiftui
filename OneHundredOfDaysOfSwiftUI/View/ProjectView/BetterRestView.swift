b85fc3c1a30e761efa02d5a082135e8a2e20cc0b
james26k <cb26.hayashi@gmail.com>
2022-02-17 00:50:21 +0900

    update: [project4] 各子Viewを囲っていたVStackをFormに差し替え

--- OneHundredOfDaysOfSwiftUI/View/ProjectView/BetterRestView.swift
+++ OneHundredOfDaysOfSwiftUI/View/ProjectView/BetterRestView.swift
@@ -25,29 +25,36 @@ struct BetterRestView: View {
     }
 
     var body: some View {
-        VStack {
-            Text("When do you want to wake up?")
-                .font(.headline)
+        Form {
+            VStack(alignment: .leading, spacing: 5) {
+                Text("When do you want to wake up?")
+                    .font(.headline)
 
-            DatePicker("Please enter a time",
-                       selection: $wakeUp,
-                       displayedComponents: .hourAndMinute)
-                .labelsHidden()
+                DatePicker("Please enter a time",
+                           selection: $wakeUp,
+                           displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+            }
 
-            Text("Desired amount of sleep")
-                .font(.headline)
+            VStack(alignment: .leading, spacing: 5) {
+                Text("Desired amount of sleep")
+                    .font(.headline)
 
-            Stepper("\(sleepAmount.formatted()) hours",
-                    value: $sleepAmount,
-                    in: 4...12,
-                    step: 0.25)
+                Stepper("\(sleepAmount.formatted()) hours",
+                        value: $sleepAmount,
+                        in: 4...12,
+                        step: 0.25)
+            }
 
-            Text("Daily coffee intake")
-                .font(.headline)
+            VStack(alignment: .leading, spacing: 5) {
+                Text("Daily coffee intake")
+                    .font(.headline)
 
-            Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
-                    value: $coffeeAmount,
-                    in: 1...20)
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+                        value: $coffeeAmount,
+                        in: 1...20)
+
+            }
         }
         .alert(alertTitle, isPresented: $showingAlert) {
             Button("OK") {}
